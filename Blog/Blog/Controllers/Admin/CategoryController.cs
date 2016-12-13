2e68a607d02a7d5f94fc53f0518572796c21d495
boykostoyanoff <boykostoyanoff@gmail.com>
2016-12-13 15:54:37 +0200

    Category
    Add Create and Edit Category

---
+++ Blog/Blog/Controllers/Admin/CategoryController.cs
@@ -0,0 +1,98 @@
+﻿using Blog.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Blog.Controllers.Admin
+{
+    public class CategoryController : Controller
+    {
+        // GET: Category
+        public ActionResult Index()
+        {
+            return RedirectToAction("List");
+        }
+
+        [HttpGet]
+        public ActionResult List()
+        {
+            using (var database = new BlogDbContext())
+            {
+                var categories = database
+                    .Categories
+                    .ToList();
+
+                return View(categories);
+            }
+        }
+
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(Category category)
+        {
+            if (ModelState.IsValid)
+            {
+                using (var database = new BlogDbContext())
+                {
+                    database.Categories.Add(category);
+                    database.SaveChanges();
+
+                    return RedirectToAction("Index");
+                }
+            }
+
+            return View(category);
+        }
+
+        [HttpGet]
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
+            using (var database = new BlogDbContext())
+            {
+                var category = database
+                    .Categories
+                    .FirstOrDefault(c => c.Id == id);
+
+                if (category == null)
+                {
+                    return HttpNotFound();
+                }
+
+                return View(category);
+
+            }            
+        }
+
+        [HttpPost]
+        public ActionResult Edit(Category category)
+        {
+
+            if (ModelState.IsValid)
+            {
+                using (var database = new BlogDbContext())
+                {
+                    database.Entry(category).State = EntityState.Modified;
+                    database.SaveChanges();
+
+                    return RedirectToAction("Index");
+                }
+            }
+                
+            return View(category);
+        }
+    }
+}
